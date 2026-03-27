4fbea8d322d5dce84c87896ffb12efb7052d236b
Tech-Matt <mattia.rizzo.un@gmail.com>
2026-03-27 23:48:57 +0100

    Added basic directory tree. Started working on implementing basic operations

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+// remember that integer math operations are included in <stdlib.h>
+
+int main() {
+    double a = 100.0;
+    double res = sqrt(a);
+    printf("Result of sqrt(100.0) = %f", res);
+    return 0;
+}
---
+++ src/ops.h
@@ -0,0 +1,55 @@
+// ops.h
+// I am going to put in this file implementation of common math operations performed in transformer neural networks
+// (For non-matrix operations like sqrt(), and other operations related to scalars, i am just going to use math.h for now)
+
+// List of required operations to be implemented
+//
+// Embedding:
+// - sqrt() (already defined in math.h)
+// - spawn a matrix[]
+//
+// Positional Encoding:
+// - spawn a matrix []
+// - log() (already defined in math.h)
+// - sin() (already defined in math.h)
+// - cos() (already defined in math.h)
+// - Operations on matrix (add, mul, div, sub, dot product)
+//
+// Dropout Layers:
+// - spawn or edit matrix []
+// - random int generator
+// 
+// Layer Normalization:
+// - Mean() on matrices
+// - Std() on matrices
+//
+// Feed Forward Network:
+// - Linear layer (whatever that does in itself)
+// - Relu() on Vectors / Matrices
+// 
+// Multi Head Attention:
+// - Matrix transpose
+// - softmax() to matrix / vector
+//
+// Residual connections:
+// - nothing new
+//
+// Encoder Block:
+// - nothing new
+//
+// Decoder Block:
+// - nothing new
+//
+// Linear Layer:
+// - log_softmax()
+//
+// Transformer block:
+// - nothing new
+//
+
+#ifndef __OPS_H__
+#define __OPS_H__
+
+
+
+#endif
